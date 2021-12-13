19dd30c9048484bc960b06e97ef4bc6afa686a16
Shawn Kebel <fxrender@outlook.com>
2021-12-12 18:32:37 -0600

    :zap:  DB connection details

--- app/config.js
+++ app/config.js
@@ -1,4 +1,8 @@
 export default {
   baseUrl: process.env.BASE_URL || "http://localhost",
+  db: {
+    name: "trailHikesDB",
+    uri: process.env.MONGODB_URI,
+  },
   port: process.env.PORT || 3000,
 };
