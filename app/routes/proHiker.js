502a704f8ee2a9e6af97f60c35da953033215925
Shawn Kebel <fxrender@outlook.com>
2021-12-15 21:44:26 -0600

    :sparkles: Validate `ProHiker`

--- app/routes/proHiker.js
+++ app/routes/proHiker.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import proHikerController from "../controllers/proHiker.js";
+import ProHiker from "../models/users/ProHiker.js";
 
 const router = new Router();
 
@@ -7,16 +8,23 @@ router.get("/", (_, res) => {
   res.send("Hello Hiking World!");
 });
 
+// ProHiker route
 router.post("/register", async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const proHiker = new ProHiker(req.body);
 
-    await proHikerController.create(username, password);
+    const errors = proHiker.validate();
 
-    // Log in the proHiker and wait for the JWT
-    const token = await proHikerController.show(username, password);
+    if (errors.length) {
+      throw new Error(errors.join("\n"));
+    }
 
-    res.send(token);
+    await proHikerController.create(proHiker);
+
+    // Log in the User and wait for the JWT
+    const token = await proHikerController.show(proHiker);
+
+    res.status(201).send(token);
   } catch ({ message }) {
     res.status(400).json({ message });
   }
