8cadce85917cbf173730953392ab42f5c220d19b
Shawn Kebel <fxrender@outlook.com>
2021-12-16 09:02:53 -0600

    :coffin: Unused code

--- app/controllers/hiker.js
+++
@@ -1,9 +0,0 @@
-import client from "../client.js";
-import config from "../config.js";
-
-const hikersClient = client.db(config.db.name).collection("hiker");
-export default {
-  index() {
-    return hikersClient.find().toArray();
-  },
-};
